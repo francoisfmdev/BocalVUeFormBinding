fd4b321d1c4e9de55e6851e3eafbd7400dbf7366
Francois MAESTRATI <francois.maestrati@gmail.com>
2020-09-27 17:50:28 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var vm = new Vue({
+  el: "#app",
+  data: {
+    firstName: "François",
+    lastName: "Maestrati",
+    age: 32,
+    adress: "Ici quelque part ",
+    post: "Dévelopeur fullstack et formateur",
+    img: "fm.jpg",
+  },
+});
